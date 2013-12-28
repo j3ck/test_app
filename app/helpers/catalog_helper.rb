09d9a653117468c00c453f9c9be0041aa76bfb05
j3ck <fox_raiodin@mail.ru>
2013-12-28 15:13:54 +0300

    add comments_count def

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -1,2 +1,13 @@
 module CatalogHelper
+	def comments_count(book)
+      count = 0
+      unless book.comments.empty?
+        book.comments.each do |comment|
+          if comment.premoderation
+            count += 1
+          end
+        end
+      end
+    	count
+    end
 end
