1bd5dc545586cc98b6f8e8b0e0a89d9f30dbaab5
j3ck <fox_raiodin@mail.ru>
2013-12-28 16:58:57 +0300

    add helper for list other author in book pages

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,12 @@
 module BooksHelper
+	def other_books_current_author(author,id)
+		oth = Array.new 
+		@books = Book.all
+		@books.each do |book|
+			if (book.author == author) && (book.id != id)
+				oth.push(book)
+			end
+		end
+		oth
+	end
 end
