16a7a51014bca9cd68be42fa7c2e19a7494ed741
j3ck <fox_raiodin@mail.ru>
2013-12-30 11:51:04 +0300

    fix user manage for admin

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,27 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+	skip_before_filter :require_no_authentication
 	authorize_resource :class => false
+
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      yield resource if block_given?
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        respond_to do |format|
+	      format.html { redirect_to users_url }
+	      format.json { head :no_content }
+	    end
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	load_and_authorize_resource
-	before_filter :authenticate_user!
+	#before_filter :authenticate_user!
 	before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 	def index
@@ -11,36 +11,23 @@ class UsersController < ApplicationController
   	end
 
 	def new
-	@user = User.new
-	end
-
-	def create
-	@user = User.create(user_params)
-	respond_to do |format|
-	  if @user.save
-	    format.html { redirect_to users_url, notice: 'Comment was successfully created.' }
-	    format.json { render action: 'show', status: :created, location: @user }
-	  else
-	    format.html { render action: 'new' }
-	    format.json { render json: @user.errors, status: :unprocessable_entity }
-	  end
-	end
+		@user = User.new
 	end
 
 	def edit
 	end
 
 	def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to users_url, notice: 'Comment was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	    respond_to do |format|
+	      if @user.update(user_params)
+	        format.html { redirect_to users_url, notice: 'User was successfully updated.' }
+	        format.json { head :no_content }
+	      else
+	        format.html { render action: 'edit' }
+	        format.json { render json: @user.errors, status: :unprocessable_entity }
+	      end
+	    end
+  	end
 
   private
     # Use callbacks to share common setup or constraints between actions.
