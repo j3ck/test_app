e69ca76e81235fb1b00bf6b7f7369679d3ebf152
j3ck <fox_raiodin@mail.ru>
2014-01-03 17:28:29 +0300

    paperclip configurate

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,11 @@
 class Book < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
+	has_attached_file :avatar, :styles => { :small => "70x96>", :large => "200x306>" }, :default_url => "/images/:style/missing.png"
 	
 	validates :title, :description, :author, presence: true
 	validates :title, :author, length: { minimum: 3, maximum: 150 }
 	validates :description, length: { minimum: 3 }
-
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	validates_attachment_size :avatar, :less_than => 1.megabytes
 
 	def self.search(search)
 		if search
