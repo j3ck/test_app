1c865fc98ba1ce9c0d0f0fe012d168a5cf8a259e
j3ck <fox_raiodin@mail.ru>
2014-01-13 22:27:10 +0300

    controllers clean

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,54 +3,37 @@ class CommentsController < ApplicationController
   before_filter :authenticate_user!, :except => [:show, :new, :create]
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
-  # GET /comments
-  # GET /comments.json
   def index
     @comments = Comment.order("premoderation ASC")
   end
 
-  # GET /comments/1
-  # GET /comments/1.json
   def show
   end
 
-  # GET /comments/new
   def new
     @comment = Comment.new(:parent_id => params[:parent_id], :book_id => params[:book_id])
     form_html = render_to_string( :partial => 'comments/add_comment', :formats => [:html], :locals => { :comment => @comment } )
     render :json => { :form_html => form_html }
   end
 
-  # GET /comments/1/edit
   def edit
   end
 
-  # POST /comments
-  # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
-    #respond_to do |format|
     is_create = @comment.save
       if is_create
         comment_html = render_to_string( :partial => 'comments/comment', :formats => [:html], :locals => { :comment => @comment } )
         form_html = render_to_string( :partial => 'comments/add_comment',
                                       :formats => [:html],
                                       :locals => { :comment => @comment.book.comments.build } )
-      #  format.html { redirect_to @comment.book, notice: 'Comment was successfully created.' }
-      #  format.json { render action: 'show', status: :created, location: @comment }
-      #else
-      #  format.html { redirect_to @comment.book }
-      #  format.json { render json: @comment.errors, status: :unprocessable_entity }
-          else
-      form_html = render_to_string( :partial => 'comments/add_comment', :formats => [:html], :locals => { :comment => @comment } )
+      else
+        form_html = render_to_string( :partial => 'comments/add_comment', :formats => [:html], :locals => { :comment => @comment } )
       end
-    #end
     render :json => { :create_status => is_create, :form_html => form_html,
                       :comment_html => comment_html, :book_id => @comment.book_id }
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
     respond_to do |format|
       if @comment.update(comment_params)
@@ -63,8 +46,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment.destroy
     respond_to do |format|
@@ -74,12 +55,10 @@ class CommentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
+    
     def comment_params
       params.require(:comment).permit(:email, :name, :content, :premoderation, :book_id, :parent_id)
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
 	load_and_authorize_resource
-	#before_filter :authenticate_user!
 	before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 	def index
@@ -38,12 +37,10 @@ class UsersController < ApplicationController
   	end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:username, :email, :role, :password, :password_confirmation)
     end
